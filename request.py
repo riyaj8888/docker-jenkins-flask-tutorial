4bb0b2e70773a1552d87669025e9600cec1bbc72
riyaj8888 <riyaj8888@gmail.com>
2022-10-25 15:14:41 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 # Change the value of experience that you want to test
 url = 'http://192.168.99.100:5000/api'
-feature = [[5.8, 4.0, 1.2, 0.2]]
+feature = [[5.8, 4.0, 1.5, 0.2]]
 labels ={
   0: "setosa",
   1: "versicolor",
